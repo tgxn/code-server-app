03ae1217ae7d0f05b0ea5c402a7e3e25942a250b
tgxn <domenic@tgxn.net>
2019-11-26 23:55:09 +0800

    update with selector and url etc

--- main.js
+++ main.js
@@ -1,89 +1,128 @@
 // Modules to control application life and create native browser window
-const { app, Menu, BrowserWindow, globalShortcut } = require("electron");
+const { app, Menu, BrowserWindow, BrowserView, globalShortcut } = require("electron");
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const Store = require('electron-store');
+const store = new Store();
+
+const prompt = require('electron-prompt');
+
 let mainWindow;
 
 function createWindow() {
+
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
     fullscreen: false,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: false
+    },
   });
 
-  globalShortcut.register('f5', function() {
-		mainWindow.reload()
-	})
-	globalShortcut.register('CommandOrControl+R', function() {
-		mainWindow.reload()
-	})
+  mainWindow.setMenu(null)
 
-  // and load the index.html of the app.
-  mainWindow.loadURL("https://localhost:8443/");
+  globalShortcut.register('f5', function () {
+    mainWindow.reload()
+  })
+  globalShortcut.register('CommandOrControl+R', function () {
+    mainWindow.reload()
+  })
+
+  let appUrl = store.get('appUrl');
+  console.log('got appUrl', appUrl);
+
+  if (appUrl) {
+    mainWindow.loadURL(appUrl);
+  } else {
+
+    prompt({
+      title: 'Coder Server URL',
+      label: 'URL:',
+      value: 'https://code.tgxn.tech/',
+      inputAttrs: {
+        type: 'url'
+      },
+      type: 'input'
+    }).then(result => {
+      if (result === null) {
+        console.log('user cancelled');
+      } else {
+        console.log('result', result);
+
+        store.set('appUrl', result);
+        // appUrl = result;
+
+        mainWindow.loadURL(appUrl);
+        mainWindow.reload()
+
+      }
+    }).catch(console.error);
+
+  }
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on("closed", function() {
+  mainWindow.on("closed", function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    Electron.session.defaultSession.clearCache(() => {})
+    // Electron.session.defaultSession.clearCache(() => { })
     mainWindow = null;
   });
-  // Create the Application's main menu
-  const template = [
-    {
-      label: "Application",
-      submenu: [
-        {
-          label: "About Application",
-          selector: "orderFrontStandardAboutPanel:"
-        },
-        { type: "separator" },
-        {
-          label: "Quit",
-          accelerator: "Command+Q",
-          click: function() {
-            app.quit();
-          }
-        }
-      ]
-    },
-    {
-      label: "Edit",
-      submenu: [
-        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-        { type: "separator" },
-        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-        {
-          label: "Select All",
-          accelerator: "CmdOrCtrl+A",
-          selector: "selectAll:"
-        }
-      ]
-    }
-  ];
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+  app.on('login', function (event, webContents, request, authInfo, callback) {
+    event.preventDefault();
+
+    let prompts = [
+      prompt({
+        title: 'Password',
+        label: 'Password:',
+        value: '',
+        inputAttrs: {
+          type: 'password'
+        },
+        type: 'input'
+      })
+    ];
+
+    let user = store.get('appUser1');
+
+    if (!user) {
+      prompts.push(prompt({
+        title: 'Username',
+        label: 'Username:',
+        value: '',
+        inputAttrs: {
+          type: 'text'
+        },
+        type: 'input'
+      }));
+    } else {
+      prompts.push(Promise.resolve(user));
+    }
+
+    Promise.all(prompts).then(results => {
+
+      store.set('appUser1', results[1]);
+
+      console.log(results[1], results[0]);
+
+      callback(results[1], results[0]);
+
+    }).catch(console.error);
+
+  });
+
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on("window-all-closed", function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== "darwin") {
@@ -91,7 +130,7 @@ app.on("window-all-closed", function() {
   }
 });
 
-app.on("activate", function() {
+app.on("activate", function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -108,6 +147,3 @@ app.on(
     callback(true);
   }
 );
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
